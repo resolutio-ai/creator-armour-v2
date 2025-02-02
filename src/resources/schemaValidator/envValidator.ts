432bcdd64e714334af459163aa448090c62f5d03
king-Alex-d-great <ogubuikealex@gmail.com>
2025-02-02 13:59:35 +0100

    Feat: Add Environment Variable Validator

---
+++ src/resources/schemaValidator/envValidator.ts
@@ -0,0 +1,8 @@
+import * as Yup from 'yup';
+
+export const configurationValidation = Yup.object({
+    LIGHTHOUSE_API_KEY: Yup.string().required("LIGHT_HOUSE_API_KEY is required").label("LIGHT HOUSE API Key").trim(),
+    MONGO_URI: Yup.string().required("MONGO_URL is required").label("MONGO API Key").trim()
+})
+
+
